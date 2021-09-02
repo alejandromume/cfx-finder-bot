34507553c403c1c48dc0e567e9ce2cd92f157c2d
Mume <43515908+alejandromume@users.noreply.github.com>
2021-09-02 20:28:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,87 +19,96 @@ bot.on('message', msg => {
         var code = args3[1]
         var urlfivem = "https://servers-frontend.fivem.net/api/servers/single/"+code
         https.get(urlfivem, function(res) {
-            console.log(res.statusCode)
-            if(res.statusCode == 404 || res.statusCode == 403){
+
+            if(res.statusCode == 404){
 
                 const mensaje = new Discord.MessageEmbed()
                 .setColor("#c73e10")
                 .setAuthor(msg.author.tag, msg.author.avatarURL())
                 .setDescription("```\n Invalid Code```")
-                .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟬𝟴𝟴𝟰")
+                .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟐𝟎𝟔𝟒")
                 msg.channel.send(mensaje);
 
             }else{
+
                 fetch(urlfivem)
                 .then(res => res.json())
                 .then((out) => {
-                    
-                    if(!out["Data"]["connectEndPoints"][0].startsWith("http")){
-        
-                        var split =  `${out["Data"]["connectEndPoints"][0]}`.split(":")
-                        var urlip = "http://ip-api.com/json/"+split[0]
+
+                    if(!out["Data"]["connectEndPoints"][0].startsWith("http")) {
+
+                        var split = `${out["Data"]["connectEndPoints"][0]}`.split(":")
+                        var urlip = "http://ip-api.com/json/" + split[0]
                         fetch(urlip)
-                        .then(res => res.json())
-                        .then((out2) => {
-                        
-                        if(out["icon"]){
-                            var icon = out2["icon"]
-                            let file = new Buffer.from(icon, 'base64')
-                            const att = new Discord.MessageAttachment(file, "graph.png")
-                            const mensaje = new Discord.MessageEmbed()
+                            .then(res => res.json())
+                            .then((out2) => {
 
-                            .setColor("#c73e10")
-                            .setAuthor(msg.author.tag, msg.author.avatarURL())
-                            .addField("IP:Port", `\`${out["Data"]["connectEndPoints"][0]}\``)
-                            .addField("Server Details", `IP: \`${split[0]}\`\n Country: \`${out2["country"]}\`\n City: \`${out2["city"]}\`\n ISP: \`${out2["isp"]}\`\n Org: \`${out2["org"]}\`\n Zip Code: \`${out2["zip"]}\`\n Timezone: \`${out2["timezone"]}\`\n`)
-                            .addField("FiveM Server", `Server Name: \`${out["Data"]["hostname"].substring(0, 390)}\`\n\n Online Players: \`${out["Data"]["players"].length}\`\n Max Players: \`${out["Data"]["svMaxclients"]}\`\n Artifacts: \`${out["Data"]["server"]}\`\n Resources: \`${out["Data"]["resources"].length}\`\n Onesync Enabled?: \`${out["Data"]["vars"]["onesync_enabled"]}\`\n`, true)
-                            .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟬𝟴𝟴𝟰")
-                            .setThumbnail("attachment://graph.png")
-                            .attachFiles(att)
+                                if (out["icon"]) {
+                                    var icon = out2["icon"]
+                                    let file = new Buffer.from(icon, 'base64')
+                                    const att = new Discord.MessageAttachment(file, "graph.png")
+                                    const mensaje = new Discord.MessageEmbed()
 
-                            msg.channel.send(mensaje);
-                        }else{
-                            const mensaje = new Discord.MessageEmbed()
-                            .setColor("#c73e10")
-                            .setAuthor(msg.author.tag, msg.author.avatarURL())
-                            .addField("IP:Port", `\`${out["Data"]["connectEndPoints"][0]}\``)
-                            .addField("Server Details", `IP: \`${split[0]}\`\n Country: \`${out2["country"]}\`\n City: \`${out2["city"]}\`\n ISP: \`${out2["isp"]}\`\n Org: \`${out2["org"]}\`\n Zip Code: \`${out2["zip"]}\`\n Timezone: \`${out2["timezone"]}\`\n`)
-                            .addField("FiveM Server", `Server Name: \`${out["Data"]["hostname"].substring(0, 390)}\`\n\n Online Players: \`${out["Data"]["players"].length}\`\n Max Players: \`${out["Data"]["svMaxclients"]}\`\n Artifacts: \`${out["Data"]["server"]}\`\n Resources: \`${out["Data"]["resources"].length}\`\n Onesync Enabled?: \`${out["Data"]["vars"]["onesync_enabled"]}\`\n`, true)
-                            .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟬𝟴𝟴𝟰")
-                            msg.channel.send(mensaje);
-                        }
-                        
-                    
-                        })
-        
+                                        .setColor("#c73e10")
+                                        .setAuthor(msg.author.tag, msg.author.avatarURL())
+                                        .addField("IP:Port", `\`${out["Data"]["connectEndPoints"][0]}\``)
+                                        .addField("Server Details", `IP: \`${split[0]}\`\n Country: \`${out2["country"]}\`\n City: \`${out2["city"]}\`\n ISP: \`${out2["isp"]}\`\n Org: \`${out2["org"]}\`\n Zip Code: \`${out2["zip"]}\`\n Timezone: \`${out2["timezone"]}\`\n`)
+                                        .addField("FiveM Server", `Server Name: \`${out["Data"]["hostname"].substring(0, 390)}\`\n\n Online Players: \`${out["Data"]["players"].length}\`\n Max Players: \`${out["Data"]["svMaxclients"]}\`\n Artifacts: \`${out["Data"]["server"]}\`\n Resources: \`${out["Data"]["resources"].length}\`\n Onesync Enabled?: \`${out["Data"]["vars"]["onesync_enabled"]}\`\n`, true)
+                                        .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟐𝟎𝟔𝟒")
+                                        .setThumbnail("attachment://graph.png")
+                                        .attachFiles(att)
+
+                                    msg.channel.send(mensaje);
+                                } else {
+                                    const mensaje = new Discord.MessageEmbed()
+                                        .setColor("#c73e10")
+                                        .setAuthor(msg.author.tag, msg.author.avatarURL())
+                                        .addField("IP:Port", `\`${out["Data"]["connectEndPoints"][0]}\``)
+                                        .addField("Server Details", `IP: \`${split[0]}\`\n Country: \`${out2["country"]}\`\n City: \`${out2["city"]}\`\n ISP: \`${out2["isp"]}\`\n Org: \`${out2["org"]}\`\n Zip Code: \`${out2["zip"]}\`\n Timezone: \`${out2["timezone"]}\`\n`)
+                                        .addField("FiveM Server", `Server Name: \`${out["Data"]["hostname"].substring(0, 390)}\`\n\n Online Players: \`${out["Data"]["players"].length}\`\n Max Players: \`${out["Data"]["svMaxclients"]}\`\n Artifacts: \`${out["Data"]["server"]}\`\n Resources: \`${out["Data"]["resources"].length}\`\n Onesync Enabled?: \`${out["Data"]["vars"]["onesync_enabled"]}\`\n`, true)
+                                        .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟐𝟎𝟔𝟒")
+                                    msg.channel.send(mensaje);
+                                }
+
+
+                            })
+
                     }else{
                         const mensaje = new Discord.MessageEmbed()
-                        .setColor("#c73e10")
-                        .setAuthor(msg.author.tag, msg.author.avatarURL())
-                        .setDescription("```\n Cannot find server details...```")
-                        .addField("Cfx Url", `\`${out["Data"]["connectEndPoints"][0]}\``)
-                        .addField("FiveM Server", `Server Name: \`${out["Data"]["hostname"].substring(0, 390)}\`\n\n Online Players: \`${out["Data"]["players"].length}\`\n Max Players: \`${out["Data"]["svMaxclients"]}\`\n Artifacts: \`${out["Data"]["server"]}\`\n Resources: \`${out["Data"]["resources"].length}\`\n Onesync Enabled?: \`${out["Data"]["vars"]["onesync_enabled"]}\`\n`, true)
-                        .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟬𝟴𝟴𝟰")
+                            .setColor("#c73e10")
+                            .setAuthor(msg.author.tag, msg.author.avatarURL())
+                            .setDescription("```\n Cannot find server details...```")
+                            .addField("Cfx Url", `\`${out["Data"]["connectEndPoints"][0]}\``)
+                            .addField("FiveM Server", `Server Name: \`${out["Data"]["hostname"].substring(0, 390)}\`\n\n Online Players: \`${out["Data"]["players"].length}\`\n Max Players: \`${out["Data"]["svMaxclients"]}\`\n Artifacts: \`${out["Data"]["server"]}\`\n Resources: \`${out["Data"]["resources"].length}\`\n Onesync Enabled?: \`${out["Data"]["vars"]["onesync_enabled"]}\`\n`, true)
+                            .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟐𝟎𝟔𝟒")
                         msg.channel.send(mensaje);
                     }
-                })
-            }
-        
-        })
+                }).catch(() => {
+                    const mensaje = new Discord.MessageEmbed()
+                        .setColor("#c73e10")
+                        .setAuthor(msg.author.tag, msg.author.avatarURL())
+                        .setDescription("```\n Invalid Code```")
+                        .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟐𝟎𝟔𝟒")
+                    msg.channel.send(mensaje);
+
+            })
+        }
+
+    })
         
 
     }else if(msg.content.startsWith(">cip")){
         const args3 = msg.content.slice(">cip".length).split(' ');
 
         var code = args3[1]
-        var urlfivem = "https://servers-live.fivem.net/api/servers/single/"+code
+        var urlfivem = "https://servers-frontend.fivem.net/api/servers/single/"+code
         https.get(urlfivem, function(res) {
             if(res.statusCode == 404){
                 const mensaje = new Discord.MessageEmbed()
                 .setColor("#c73e10")
                 .setAuthor(msg.author.tag, msg.author.avatarURL())
                 .setDescription("```\n Invalid Code```")
-                .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟬𝟴𝟴𝟰")
+                .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟐𝟎𝟔𝟒")
                 msg.channel.send(mensaje);
             }else{
                 fetch(urlfivem)
@@ -109,8 +118,16 @@ bot.on('message', msg => {
                     .setAuthor(msg.author.tag, msg.author.avatarURL())
                     .setColor("#c73e10")
                     .addField("IP:Port", `\`${out["Data"]["connectEndPoints"][0]}\``)
-                    .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟬𝟴𝟴𝟰")
+                    .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟐𝟎𝟔𝟒")
                     msg.channel.send(mensaje);
+                }).catch(() => {
+                    const mensaje = new Discord.MessageEmbed()
+                        .setColor("#c73e10")
+                        .setAuthor(msg.author.tag, msg.author.avatarURL())
+                        .setDescription("```\n Invalid Code```")
+                        .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟐𝟎𝟔𝟒")
+                    msg.channel.send(mensaje);
+
                 })
             }
             
@@ -125,7 +142,8 @@ bot.on('message', msg => {
         .addField("\<:ip:820801530172801035> Get Server IP", "`>cip [cfx code]`")
         .addField("\<:logo:820801824050905129> Get Server Logo", "`>clogo [cfx code]`")
         .addField("\<:tags:820801679208349737> Get Server Tags", "`>ctags [cfx code]`")
-        .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟬𝟴𝟴𝟰")
+        .addField("\<:resources:863110994791235624> Get Server Resources", "`>cresources [cfx code]`")
+        .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟐𝟎𝟔𝟒")
         msg.channel.send(mensaje);
     
     }else if(msg.content.startsWith(">clogo")){
@@ -133,7 +151,7 @@ bot.on('message', msg => {
         const args3 = msg.content.slice(">clogo".length).split(' ');
 
         var code = args3[1]
-        var urlfivem = "https://servers-live.fivem.net/api/servers/single/"+code
+        var urlfivem = "https://servers-frontend.fivem.net/api/servers/single/"+code
         fetch(urlfivem)
                 .then(res => res.json())
                 .then((out) => {
@@ -159,16 +177,22 @@ bot.on('message', msg => {
         
                                     msg.channel.send(mensaje);
                                 })
-    
                         }
-                    
-                })
+                }).catch(() => {
+            const mensaje = new Discord.MessageEmbed()
+                .setColor("#c73e10")
+                .setAuthor(msg.author.tag, msg.author.avatarURL())
+                .setDescription("```\n Invalid Code```")
+                .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟐𝟎𝟔𝟒")
+            msg.channel.send(mensaje);
+
+        })
     }else if(msg.content.startsWith(">ctags")){
 
         const args3 = msg.content.slice(">clogo".length).split(' ');
 
         var code = args3[1]
-        var urlfivem = "https://servers-live.fivem.net/api/servers/single/"+code
+        var urlfivem = "https://servers-frontend.fivem.net/api/servers/single/"+code
         fetch(urlfivem)
                 .then(res => res.json())
                 .then((out) => {
@@ -182,7 +206,7 @@ bot.on('message', msg => {
                         .setColor("#c73e10")
                         .addField("Server Name", `\`${name}\``.substring(0, 390))
                         .addField("Server Tags", `\`${tags}\``.substring(0, 1024))
-                        .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟬𝟴𝟴𝟰")
+                        .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟐𝟎𝟔𝟒")
                         msg.channel.send(mensaje);
 
                     }else{
@@ -192,10 +216,84 @@ bot.on('message', msg => {
                         .setDescription("```\n Cannot find server tags```")
                         msg.channel.send(mensaje);
                     }
+                    
+                
+                }).catch(() => {
+            const mensaje = new Discord.MessageEmbed()
+                .setColor("#c73e10")
+                .setAuthor(msg.author.tag, msg.author.avatarURL())
+                .setDescription("```\n Invalid Code```")
+                .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟐𝟎𝟔𝟒")
+            msg.channel.send(mensaje);
 
-                })
-    }
+        })
+
+    }else if(msg.content.startsWith(">cresources")){
+
+        const args3 = msg.content.slice(">cresources".length).split(' ');
+
+        var code = args3[1]
+        var urlfivem = "https://servers-frontend.fivem.net/api/servers/single/"+code
+        fetch(urlfivem)
+                .then(res => res.json())
+                .then((out) => {
+                    if(out["Data"]["resources"] && out["Data"]["hostname"]){
+
+                        var resources = out["Data"]["resources"]
+                        var name = out["Data"]["hostname"]
+
+                        const mensaje = new Discord.MessageEmbed()
+                        .setAuthor(msg.author.tag, msg.author.avatarURL())
+                        .setColor("#c73e10")
+                        .addField("Server Name", `\`${name}\``.substring(0, 390))
+                        .addField("Server Resources", `\`${resources}\``.substring(0, 1024))
+                        .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟐𝟎𝟔𝟒")
+                        msg.channel.send(mensaje);
+
+                    }else{
+                        const mensaje = new Discord.MessageEmbed()
+                        .setAuthor(msg.author.tag, msg.author.avatarURL())
+                        .setColor("#c73e10")
+                        .setDescription("```\n Cannot find server tags```")
+                        msg.channel.send(mensaje);
+                    }
+                    
+                
+                }).catch(() => {
+            const mensaje = new Discord.MessageEmbed()
+                .setColor("#c73e10")
+                .setAuthor(msg.author.tag, msg.author.avatarURL())
+                .setDescription("```\n Invalid Code```")
+                .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟐𝟎𝟔𝟒")
+            msg.channel.send(mensaje);
+
+        })
     
+    }else if(msg.content.startsWith(">cowner")){
+        const args3 = msg.content.slice(">cowner".length).split(' ');
+        var code = args3[1]
+        var urlfivem = "https://servers-frontend.fivem.net/api/servers/single/"+code
+        fetch(urlfivem)
+        .then(res => res.json())
+        .then((out) => {
+            if(out["Data"]["ownerName"]){
+                const mensaje = new Discord.MessageEmbed()
+                .setAuthor(msg.author.tag, msg.author.avatarURL())
+                .setColor("#c73e10")
+                .addField("Owner Name", `\`${out["Data"]["ownerName"]}\``)
+                .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟐𝟎𝟔𝟒")
+                 msg.channel.send(mensaje);
+            }
+        }).catch(() => {
+            const mensaje = new Discord.MessageEmbed()
+                .setColor("#c73e10")
+                .setAuthor(msg.author.tag, msg.author.avatarURL())
+                .setDescription("```\n Invalid Code```")
+                .setFooter("Coded by 𝗮𝗹𝗲𝗷𝗮𝗻𝗱𝗿𝗼𝗺𝘂𝗺𝗲#𝟐𝟎𝟔𝟒")
+            msg.channel.send(mensaje);
+
+        })
+    }
 
 })
 
